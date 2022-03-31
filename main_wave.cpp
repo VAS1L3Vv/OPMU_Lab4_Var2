292bf5aa82a91975dc3a9da09347960135584d14
VAS1L3Vv <viktorborayuceil@hotmail.com>
2022-03-31 12:59:18 +0300

    Разработка шаблона в мейн

--- main_wave.cpp
+++ main_wave.cpp
@@ -1,6 +1,11 @@
 #include "wave.h"
-
+#include <iostream>
+using std::cout;
 int main()
 {
-
+	int a;
+	float b;
+	cout << "¬ведите с новой строки: „астоту дискретизации, количество отсчетов (или оставьте по умолчанию = 48к√ц; =512;) \n";
+	cin >> a; 
+	cin >> b;
 }
