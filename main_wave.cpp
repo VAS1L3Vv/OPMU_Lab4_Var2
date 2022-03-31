904e726d94e83c9b97caf22f5b148f003d76a45c
VAS1L3Vv <viktorborayuceil@hotmail.com>
2022-03-31 14:01:03 +0300

    разработка мейн 1

--- main_wave.cpp
+++ main_wave.cpp
@@ -1,11 +1,19 @@
 #include "wave.h"
 #include <iostream>
 using std::cout;
+using std::cin;
 int main()
 {
-	int a;
+	vector <complex<float>> 
+	unsigned int a;
 	float b;
 	cout << "¬ведите с новой строки: „астоту дискретизации, количество отсчетов (или оставьте по умолчанию = 48к√ц; =512;) \n";
 	cin >> a; 
 	cin >> b;
+	Wave SIGNAL(a,b);
+	SIGNAL.setDataSize(a);
+	SIGNAL.GenerateSignal();
+	SIGNAL.CalculateFFT();
+
+
 }
