ebb0fa59d1491c701e3cb4488b4971ce3b0175db
VAS1L3Vv <viktorborayuceil@hotmail.com>
2022-03-31 11:36:01 +0300

    Созданые геттеры и сеттеры членов, расчет гармоники сигнала

--- wave.h
+++ wave.h
@@ -1,10 +1,15 @@
+#pragma once
+#ifndef _WAVE_H
+#define _WAVE_H
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
 #include <complex>
 #include <vector>
 #include <fftw3.h>
-
-fftwf_plan fftwf_plan_dft_1d(int n, fftwf_complex* sample_in, fftwf_complex* sample_out, fftwf_direction direction, int flags);
+#include <cmath>
+using std::vector;
+using std::complex;
 struct ADCFILEHEADER
 {
 	double dt;
@@ -19,19 +24,45 @@ typedef struct ADCFILEHEADER* LPADCFILEHEADER;
 #define ADCF_DATA_FLOAT 0x00000001
 #define ADCF_TYPE_REAL 0x00000000 
 #define ADCF_TYPE_COMPLEX 0x00000002
+// head.flags = ADCF_TYPE_COMPLEX | ADCF_DATA_FLOAT;
 class Wave
 {
 private:
-	unsigned int SizeFFT = 512;
-	std::vector <float> f = { 0.0,200.0,1024.0,400.0 }; //Создан массив из 4 элементов для удобного индексирования гармоник где f1 = f[1]..... fn = f[n]
-
-			// ПРИМЕЧАНИЕ: ПО СПЕЦИФИКЕ ВАРИАНТА, ЧАСТОТЫ f1 и f3 СОВПАДАЮТ, ПОЭТОМУ БЫЛО ВЗЯТО f3 = 400 Hz.
-	std::vector <float> A = {0, 0.02, 0.2, 0.2}; // Создан массив из 4 элементов для удобного индексирования амплитуд гармоник, где А1 = А[1]..... An = A[n]
-
-public:
-	int getSizeFFT()
+	Wave(unsigned int size = 512, float frqsmpl = 48000.0)
 	{
-		return SizeFFT;
+		FFT_length = size;
+		frq_sample = frqsmpl;
+	}
+	// ПРИМЕЧАНИЕ: ПО СПЕЦИФИКЕ ВАРИАНТА, ЧАСТОТЫ f1 и f3 СОВПАДАЮТ, ПОЭТОМУ БЫЛО ВЗЯТО f3 = 400 Hz.
+
+	float frq_sample;
+	unsigned int FFT_length;
+	vector <complex<float>> signal_data;			// вектор отсчета сигналов
+	vector <float> f = { 0.0,200.0,1024.0,400.0 }; //Создан массив из 4 элементов для удобного индексирования гармоник где f1 = f[1]..... fn = f[n]
+	vector <float> A = { 0, 0.02, 0.2, 0.2 };		  // Создан массив из 4 элементов для удобного индексирования амплитуд гармоник, где А1 = А[1]..... An = A[n]											  
+
+	complex <float> SignalHarmony(int n) // Метод, возвращающий расчет сигнала соответсвующей гармоники
+	{
+		return sin(2 * M_PI * n * f[n] / frq_sample);
+	}
+	void setFFT_length(unsigned int set) // приватный сеттер для косвенного применения в другом публичном методе
+	{
+		FFT_length = set;
+	}
+	int getFFT_length() // приватный геттер для косвенного применения в другом публичном методе
+	{
+		return FFT_length;
 	}
 
+public:
+	void setDataSize(unsigned int set)
+	{
+		setFFT_length(set);
+		signal_data.resize(getFFT_length());
+	}
+	vector <complex<float>> getSignalData()
+	{
+		return signal_data;
+	}
 };
+#endif // !_WAVE_H
